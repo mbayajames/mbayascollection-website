9e393378d03ee3d135ce2318cbf019e899551953
mbayajames <jmuthiora244@gmail.com>
2025-06-08 01:35:26 -0700

    Refactor analytics utilities in src/utils/analytics.js for better structure and maintainability

---
+++ src/utils/analytics.js
@@ -0,0 +1,9 @@
+export function trackProductClick(productId) {
+  console.log(`Product clicked: ${productId}`);
+  // Mocked: Send to analytics service
+}
+
+export function trackSearch(query) {
+  console.log(`Search performed: ${query}`);
+  // Mocked: Send to analytics service
+}
